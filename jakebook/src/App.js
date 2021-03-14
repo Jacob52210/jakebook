19dcaefd618fab8b796691779907f427219d27fb
Jacob Flora <jacobflora4@gmail.com>
2021-03-14 13:21:51 -0400

    successfully connected Posts/Post and Form and imported them into App.js

--- jakebook/src/App.js
+++ jakebook/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './style.css';
+
+//import logo from './images/';
+import Posts from './components/Posts/Posts.js'
+import Form from './components/Form/Form.js'
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+   <div className="app-container">
+     <header>
+       <h1>Jakebook</h1>
+     </header>
+     <div className="content-container">
+
+        <div className="form-container">
+          <Form />
+        </div>
+
+        <div className="posts-container">
+          <Posts />
+        </div>
+        
+     </div>
+   </div>
   )
 }
 
